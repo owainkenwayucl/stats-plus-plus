391b2f64c0ad7a1d77d6525b733674a026389039
Owain Kenway <o.kenway@ucl.ac.uk>
2017-08-08 15:41:23 +0100

    Simple template functionality for R

---
+++ r/simpletemplate.r
@@ -0,0 +1,15 @@
+# Replace values in a string with values from a vector where names(keys)
+# contains the keys, and the keys contains the values.
+templatestring <- function(s, keys) {
+  r <- s
+  for (a in names(keys)) {
+    r <- gsub(a, keys[a], r)
+  }
+  return(r)
+}
+
+# Version with file instead of string.
+templatefile <- function(filename, keys) {
+  x <- readChar(filename, file.info(filename)$size)
+  return(templatestring(x, keys))
+}
