256eef054e40c25fb119118e65b6294d4161a123
Owain Kenway <o.kenway@ucl.ac.uk>
2017-08-10 11:43:39 +0100

    Add correct wait time, tidy SQL
    Make SQL statements more readable.

--- sql/wait-by-user.sql
+++ sql/wait-by-user.sql
@@ -1,5 +1,5 @@
 SELECT end_period as period, 
        CONCAT_WS(' | ', t1.owner, fullname) AS owner, 
-       SUM(cost*run_time) AS cpu_time 
+       SUM(wait_time) AS wait_time
        FROM %DB%.accounting_view AS t1 LEFT JOIN user_info.user_names AS t2 ON t1.owner = t2.username 
        WHERE (end_time > start_time AND end_period = "%PERIOD%") GROUP BY period,t1.owner;
