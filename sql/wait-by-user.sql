937a228f646b33315c3fa4c0bf7cfdfdfb8ce198
Owain Kenway <o.kenway@ucl.ac.uk>
2017-08-10 10:27:53 +0100

    Select by period.

--- sql/wait-by-user.sql
+++ sql/wait-by-user.sql
@@ -1,1 +1,1 @@
-SELECT end_period as period, CONCAT_WS(' | ', t1.owner, fullname) AS owner, SUM(cost*run_time) AS cpu_time FROM %DB%.accounting_view AS t1 LEFT JOIN user_info.user_names AS t2 ON t1.owner = t2.username WHERE end_time > start_time GROUP BY period,t1.owner;
+SELECT end_period as period, CONCAT_WS(' | ', t1.owner, fullname) AS owner, SUM(cost*run_time) AS cpu_time FROM %DB%.accounting_view AS t1 LEFT JOIN user_info.user_names AS t2 ON t1.owner = t2.username WHERE (end_time > start_time AND end_period = "%PERIOD%") GROUP BY period,t1.owner;
