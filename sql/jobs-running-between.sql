1325fc5cba28bac55db673c4004b4e275ffdbf17
Owain Kenway <o.kenway@ucl.ac.uk>
2017-08-16 13:35:26 +0100

    SQL to find jobs running between two times.

---
+++ sql/jobs-running-between.sql
@@ -0,0 +1,7 @@
+select job_number from %DB% where 
+      ((end_time > unix_timestamp("%START%")) and 
+      (end_time < unix_timestamp("%STOP%"))) or
+      ((start_time > unix_timestamp("%START%")) and 
+      (start_time < unix_timestamp("%STOP%"))) or
+      ((start_time < unix_timestamp("%START%")) and
+      (end_time > unix_timestamp("%STOP%")))
