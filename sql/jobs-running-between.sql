9e6f6f11ae05125e3777ab06fd8575d15feac98a
Owain Kenway <o.kenway@ucl.ac.uk>
2017-08-16 13:39:28 +0100

    Code in accounting table.

--- sql/jobs-running-between.sql
+++ sql/jobs-running-between.sql
@@ -1,4 +1,4 @@
-select job_number from %DB% where 
+select job_number from %DB%.accounting where 
       ((end_time > unix_timestamp("%START%")) and 
       (end_time < unix_timestamp("%STOP%"))) or
       ((start_time > unix_timestamp("%START%")) and 
