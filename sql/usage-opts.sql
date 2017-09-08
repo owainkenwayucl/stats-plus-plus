a16ce4d0b47f8d908de9290b66766610735307e0
Owain Kenway <o.kenway@ucl.ac.uk>
2017-09-08 14:12:06 +0100

    Make more readable.

--- sql/usage-opts.sql
+++ sql/usage-opts.sql
@@ -1,1 +1,4 @@
-select sum((ru_wallclock*cost)) from %DB%.accounting where ((end_time > unix_timestamp('%START%')) and (end_time < unix_timestamp('%STOP%')) %ONLIMITS% );
+select sum((ru_wallclock*cost)) from %DB%.accounting where 
+       ((end_time > unix_timestamp('%START%')) and 
+        (end_time < unix_timestamp('%STOP%')) 
+        %ONLIMITS% );
--- sql/usage.sql
+++ sql/usage.sql
@@ -1,1 +1,3 @@
-select sum((ru_wallclock*cost)) from %DB%.accounting where ((end_time > unix_timestamp('%START%')) and (end_time < unix_timestamp('%STOP%')) );
+select sum((ru_wallclock*cost)) from %DB%.accounting where 
+       ((end_time > unix_timestamp('%START%')) and 
+        (end_time < unix_timestamp('%STOP%')) );
