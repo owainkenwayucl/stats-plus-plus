2ab38f6fa882b6cd63776c36529fac8135bf4a24
Owain Kenway <o.kenway@ucl.ac.uk>
2017-09-25 15:29:44 +0100

    Tool to print usage per user for a POC.

--- Makefile
+++ Makefile
@@ -1,7 +1,10 @@
-all: time-by-cost-by-inst
+all: time-by-cost-by-inst time-by-cost-by-inst-user
 
 time-by-cost-by-inst:
 	ln -s shell/r-wrap time-by-cost-by-inst
+
+time-by-cost-by-inst-user:
+	ln -s shell/r-wrap time-by-cost-by-inst-user
 
 clean:
 	rm time-by-cost-by-inst
---
+++ r/time-by-cost-by-inst-user.r
@@ -0,0 +1,50 @@
+#!/usr/bin/env Rscript
+args <- commandArgs(trailingOnly=TRUE)
+
+if (length(args)!=2) {
+  cat("time-by-cost-by-inst institutionYYYY-MM\n")
+  return(NA)
+}
+
+inst <- args[1]
+period <- args[2]
+
+source("r/simpletemplate.r")
+source("r/dbtools.r")
+
+
+db <- "thomas"
+dba <- "thomas_sgelogs"
+
+keys <- genkeys(c("%INSTITUTE%"), c(inst))
+query <- templatefile("sql/ist-to-users.sql", keys)
+users <- dbquery(db, query)
+
+results <- data.frame(inst = character(), single=numeric(0), optimal=numeric(0), big=numeric(0))
+
+for (a in users$username) {
+
+    userlimits <- onlimits(a)
+
+    keys <- genkeys(c("%DB%", "%PERIOD%", "%LOWERCOST%", "%UPPERCOST%", "%ONLIMITS%"), c(dba, period, "1", "24", userlimits))
+    query <- templatefile("sql/usage-cost-opts.sql", keys)
+    usage <- dbquery(dba, query)
+
+    keys <- genkeys(c("%DB%", "%PERIOD%", "%LOWERCOST%", "%UPPERCOST%", "%ONLIMITS%"), c(dba, period, "25", "120", userlimits))
+    query <- templatefile("sql/usage-cost-opts.sql", keys)
+    goodusage <- dbquery(dba, query)
+
+    keys <- genkeys(c("%DB%", "%PERIOD%", "%LOWERCOST%", "%UPPERCOST%", "%ONLIMITS%"), c(dba, period, "121", "10000000", userlimits))
+    query <- templatefile("sql/usage-cost-opts.sql", keys)
+    bigusage <- dbquery(dba, query)
+
+    tempframe <- data.frame(inst=a, single=usage, optimal=goodusage, big=bigusage)
+    
+    if ((!is.na(usage)) && (!is.na(goodusage)) && (!is.na(bigusage))){
+      results <- rbind(results, tempframe)
+    }
+
+} 
+names(results)<-c("User", "Single", "Target", "Large")
+results[is.na(results)] <- 0
+write.csv(results)
