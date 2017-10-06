28509c526a6d2212b258333242c95a92b6f3cf3c
Owain Kenway <o.kenway@ucl.ac.uk>
2017-10-06 10:59:49 +0100

    Formatting

--- sql/machine-hours-per-group.sql
+++ sql/machine-hours-per-group.sql
@@ -1,27 +1,28 @@
 # Heather wrote this and I converted it to use SimpleTemplate.
 
 SELECT COUNT(*) as num_jobs, sum(cost*ru_wallclock)/3600 AS core_hours, 
-  sum(cost*ru_wallclock)/(3600*17000) AS machine_hours
+       sum(cost*ru_wallclock)/(3600*17000) AS machine_hours
 
 FROM %DB%.accounting AS t1 
 # inner join to get only the jobs that have these users
-  INNER JOIN (
+INNER JOIN (
     # Get distinct users only and concatenate all their projects into one
-	# because we still need a project field
-    SELECT username, GROUP_CONCAT(project SEPARATOR ', ') AS proj 
+    # because we still need a project field
+    SELECT username, GROUP_CONCAT(project SEPARATOR ', ') AS proj
     FROM thomas.projectusers 
     WHERE thomas.projectusers.project LIKE '%PROJECT%%'
     GROUP BY username
-  ) AS distinctuser
+    
+    ) AS distinctuser
+    
+    ON t1.owner = distinctuser.username 
 
-  ON t1.owner = distinctuser.username 
+WHERE
+    end_time > start_time
+    AND date_format(from_unixtime(end_time),"%Y-%m") = '%PERIOD%'
 
-WHERE  
-  end_time > start_time
-  AND date_format(from_unixtime(end_time),"%Y-%m") = '%PERIOD%'
-
-  AND (
-    t1.project='Gold' AND SUBSTRING_INDEX(account, ';', 1) LIKE '%PROJECT%%' # paid jobs for this project only
-    OR t1.project='AllUsers' # any free jobs from this user
-  )
+    AND (
+        t1.project='Gold' AND SUBSTRING_INDEX(account, ';', 1) LIKE '%PROJECT%%' # paid jobs for this project only
+        OR t1.project='AllUsers' # any free jobs from this user
+    )
 
