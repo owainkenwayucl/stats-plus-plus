2f77a4ff42158cae665fa9d62b3e59adb80bb457
Owain Kenway <o.kenway@ucl.ac.uk>
2019-04-30 14:49:41 +0100

    Tidy after ourselves.

--- shell/faculty-spreadsheet.sh
+++ shell/faculty-spreadsheet.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
+set -e
+
 if [ "$#" = "2"]; then
   ./usage-by-faculty -c ${1}-d ${2}-01 > facultyin.csv
   ./miscellc -i miscell/facultyskeleton.mcl -o ${1}-${2}.csv
+  rm facultyin.csv
 else
   echo "Run with cluster name as argument 1 and YYYY-MM as argument 2"
 fi
