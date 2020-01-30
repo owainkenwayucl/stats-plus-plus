f7b157d9e90c7824ceb0d7f96f43e67e23de82a4
Owain Kenway <o.kenway@ucl.ac.uk>
2020-01-30 10:14:24 +0000

    Add Kathleen

--- sql/allusersalltime.sql
+++ sql/allusersalltime.sql
@@ -28,6 +28,11 @@ FROM (
       UNION ALL
         (SELECT DISTINCT `owner` AS `an_owner`,
                          DATE_FORMAT(FROM_UNIXTIME(end_time), "%Y-%m") AS `Period`
+         FROM kathleen_sgelogs.accounting
+         WHERE end_time != 0)
+      UNION ALL
+        (SELECT DISTINCT `owner` AS `an_owner`,
+                         DATE_FORMAT(FROM_UNIXTIME(end_time), "%Y-%m") AS `Period`
          FROM myriad_sgelogs.accounting
          WHERE end_time != 0)) AS t
 GROUP BY `Period`;
