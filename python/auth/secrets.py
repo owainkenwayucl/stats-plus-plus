b1a06b069232d9347ecf2d74fbe010948e019e4f
Owain Kenway <o.kenway@ucl.ac.uk>
2020-02-04 14:01:55 +0000

    Add AD to secrets.

--- python/auth/secrets.py
+++ python/auth/secrets.py
@@ -15,3 +15,37 @@ class Secrets:
 
         self.dbuser = secretconfig.get("database", "user").strip("'")
         self.dbpasswd = secretconfig.get("database", "pass").strip("")
+
+        # See if we have an AD password available to us anywhere:
+        self.ad = False
+
+        # First check our config file for "ad" or "ldap" sections.
+        if secretconfig.has_section("ad"):
+            self.aduser = secretconfig.get("ad", "user").strip("'")
+            self.adpasswd = secretconfig.get("ad", "pass").strip("")
+            self.ad = True
+        
+        elif secretconfig.has_section("ldap"):
+            self.aduser = secretconfig.get("ldap", "user").strip("'")
+            self.adpasswd = secretconfig.get("ldap", "pass").strip("")
+            self.ad = True
+        
+        # Then check to see if we have a ~/.adpw as for the ClusterStats repo.
+        elif os.path.exists(os.path.expanduser("~/.adpw")):
+            self.aduser = "AD\sa-ritsldap01"
+            
+            with open(os.path.expanduser("~/.adpw")) as f:
+                self.passwd = f.read().strip()
+            self.ad = True
+
+        # Then check to see if we have a system-wide password.
+        elif os.path.exists("/shared/ucl/etc/adpw"):
+            self.aduser = "AD\sa-ritsldap01"
+            
+            with open("/shared/ucl/etc/adpw") as f:
+                self.passwd= f.read().strip()
+            self.ad = True
+
+
+
+        
