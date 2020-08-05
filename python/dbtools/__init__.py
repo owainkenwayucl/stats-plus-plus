8a478d082298235814639a7150272f50eb1f75f4
Owain Kenway <o.kenway@ucl.ac.uk>
2020-08-05 11:11:10 +0100

    Add young to list of services.

--- python/dbtools/__init__.py
+++ python/dbtools/__init__.py
@@ -119,7 +119,7 @@ def undecimal(n):
 # Work out which service we are on.
 def getservice():
     import socket
-    services = ["grace", "legion", "myriad", "thomas", "kathleen"]
+    services = ["grace", "legion", "myriad", "thomas", "kathleen", "young"]
     for a in socket.gethostbyaddr(socket.gethostname()):
         for b in services:
             if b in a:
