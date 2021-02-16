84e5ed35acbc07a705f37fbf98065f0bc49ecdac
Owain Kenway <o.kenway@ucl.ac.uk>
2021-02-16 13:59:32 +0000

    First attempt at a REF query.

---
+++ sql/cputime-by-ref.sql
@@ -0,0 +1,10 @@
+SELECT
+    t1.end_period                 AS `Period`,
+    t2.department                 AS `Department`,
+    SUM(t1.cost * t1.run_time)    AS `Total CPU Time Usage`,
+    ref_cat_table.ref_category    AS ref_category
+FROM %DB%.accounting_view
+RIGHT JOIN user_info.user_refcats AS ref_cat_table 
+    ON accounting_view.owner = ref_cat_table.username 
+    WHERE end_time > start_time 
+    GROUP BY ref_cat_table.ref_category;
