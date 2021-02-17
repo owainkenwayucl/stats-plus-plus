e04aad0d73fa13c7ed908b1e836e1ef13c4632ec
Owain Kenway <o.kenway@ucl.ac.uk>
2021-02-17 14:33:57 +0000

    Yeah, this is a mess.

--- sql/cputime-by-ref.sql
+++ sql/cputime-by-ref.sql
@@ -4,6 +4,6 @@ SELECT
     ref_cat_table.ref_category    AS ref_category
 FROM %DB%.accounting_view AS t1
 RIGHT JOIN user_info.user_refcats AS ref_cat_table 
-    ON accounting_view.owner = ref_cat_table.username 
+    ON t1.owner = ref_cat_table.username 
     WHERE end_time > start_time 
     GROUP BY ref_cat_table.ref_category;
