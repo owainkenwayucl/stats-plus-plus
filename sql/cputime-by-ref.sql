1b81310f4c552da4455fa64cfa24fae7ed41a626
Owain Kenway <o.kenway@ucl.ac.uk>
2021-02-17 14:32:35 +0000

    Remove copy + paste nonsense.

--- sql/cputime-by-ref.sql
+++ sql/cputime-by-ref.sql
@@ -1,9 +1,8 @@
 SELECT
     t1.end_period                 AS `Period`,
-    t2.department                 AS `Department`,
     SUM(t1.cost * t1.run_time)    AS `Total CPU Time Usage`,
     ref_cat_table.ref_category    AS ref_category
-FROM %DB%.accounting_view
+FROM %DB%.accounting_view AS t1
 RIGHT JOIN user_info.user_refcats AS ref_cat_table 
     ON accounting_view.owner = ref_cat_table.username 
     WHERE end_time > start_time 
