72e8c0817e155450a98b37f70fe64cb34cfa8700
Owain Kenway <o.kenway@ucl.ac.uk>
2021-02-17 14:47:11 +0000

    Typo

--- sql/cputime-for-ref.sql
+++ sql/cputime-for-ref.sql
@@ -6,5 +6,5 @@ FROM %DB%.accounting_view AS t1
 RIGHT JOIN user_info.user_refcats AS ref_cat_table 
     ON t1.owner = ref_cat_table.username 
     WHERE end_time > start_time
-    AND ref_cat_tanle.ref_category = %REFCAT%
+    AND ref_cat_table.ref_category = %REFCAT%
     GROUP BY end_period;
