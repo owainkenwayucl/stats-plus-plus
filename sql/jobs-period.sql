f6504ec0f4bd12f7dfabf7674cc66891514875bd
heatherkellyucl <h.kelly@ucl.ac.uk>
2023-09-07 17:19:57 +0100

    There can be multiple entries for a job and we want the earliest submit time to find the jobscript

--- sql/jobs-period.sql
+++ sql/jobs-period.sql
@@ -1,2 +1,6 @@
-select job_number,start_time,end_time,cost from %DB%.accounting_view where 
-	(end_time > start_time AND end_period = "%PERIOD%");
+select t1.job_number,from_unixtime(min(submission_time)) as submit_time,from_unixtime(max(start_time)) as start_time, from_unixtime(t2.max_end_time) as end_time, cost from %DB%.accounting as t1 
+	join (select job_number,max(end_time) as max_end_time from %DB%.accounting
+             group by job_number) as t2 
+        on t1.job_number = t2.job_number
+	where  (date_format(from_unixtime(max_end_time),'%Y-%m') = "%PERIOD%")
+
